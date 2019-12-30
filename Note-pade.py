6cf4f32766c5ca882661ab0dc48cdd2f07d71c7b
vishal kumar <vishalkumnar0579@gmail.com>
2019-12-30 23:21:52 +0530

    note pade

---
+++ Note-pade.py
@@ -0,0 +1,174 @@
+
+from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.messagebox import askquestion
+from tkinter.messagebox import askretrycancel
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+
+def newFile():
+    global file
+    root.title("Untitled - Notepad")
+    file = None
+    TextArea.delete(1.0, END)
+
+
+def openFile():
+    global file
+    file = askopenfilename(defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+    if file == "":
+        file = None
+    else:
+        root.title(os.path.basename(file) + " - Notepad")
+        TextArea.delete(1.0, END)
+        f = open(file, "r")
+        TextArea.insert(1.0, f.read())
+        f.close()
+
+
+def saveFile():
+    global file
+    if file == None:
+        file = asksaveasfilename(initialfile = 'Untitled.txt', defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+        if file =="":
+            file = None
+
+        else:
+            #Save as a new file
+            f = open(file, "w")
+            f.write(TextArea.get(1.0, END))
+            f.close()
+
+            root.title(os.path.basename(file) + " - Notepad")
+            print("File Saved")
+    else:
+        # Save the file
+        f = open(file, "w")
+        f.write(TextArea.get(1.0, END))
+        f.close()
+def saveas():
+
+    global file
+    if file == None:
+        file = asksaveasfilename(initialfile = 'Untitled.txt', defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+        if file =="":
+            file = None
+
+        else:
+            #Save as a new file
+            f = open(file, "w")
+            f.write(TextArea.get(1.0, END))
+            f.close()
+
+            root.title(os.path.basename(file) + " - Notepad")
+            print("File Saved")
+    else:
+        # Save the file
+        f = open(file, "w")
+        f.write(TextArea.get(1.0, END))
+        f.close()
+
+
+def quitApp():
+    root.destroy()
+
+def cut():
+    TextArea.event_generate(("<<Cut>>"))
+
+def copy():
+    TextArea.event_generate(("<<Copy>>"))
+
+def paste():
+    TextArea.event_generate(("<<Paste>>"))
+
+def about():
+    showinfo("Notepad", "Notepad by VISHAL KUMAR")
+def rate():
+    value = askquestion("Rate us","Did you had a good experience")
+    if value == "yes":
+        showinfo("Rate us","Great!! Rate us App store")
+    else:
+        showinfo("Rate us","Tell us what went wrong, contact us through our servers")
+def guide():
+    top = Toplevel()
+    top.title("Guide")
+    Label(text="Made By Vishal Kumar").pack()
+def report():
+    ans = askretrycancel("connecting to server", "wait while we establish connection with the main server")
+    if ans:
+        showinfo("Report","Your Report has been approved")
+    else:
+        showinfo("Report","Servers is busy.Please try again later")
+
+
+
+if __name__ == '__main__':
+    #Basic tkinter setup
+    root = Tk()
+    root.title("Untitled - Notepad")
+    root.wm_iconbitmap("download3.ico")
+    root.geometry("444x588")
+
+    #Add TextArea
+    TextArea = Text(root, font="lucida 13")
+    file = None
+    TextArea.pack(expand=True, fill=BOTH)
+
+    # Lets create a menubar
+    MenuBar = Menu(root)
+
+    #File Menu Starts
+    FileMenu = Menu(MenuBar, tearoff=0)
+    # To open new file
+    FileMenu.add_command(label="New", command=newFile)
+
+    #To Open already existing file
+    FileMenu.add_command(label="Open", command = openFile)
+
+    # To save the current file
+
+    FileMenu.add_command(label = "Save", command = saveFile)
+    FileMenu.add_command(label="Save as", command=saveas)
+    FileMenu.add_separator()
+    FileMenu.add_command(label = "Exit", command = quitApp)
+    MenuBar.add_cascade(label = "File", menu=FileMenu)
+    # File Menu ends
+
+    # Edit Menu Starts
+    EditMenu = Menu(MenuBar, tearoff=0)
+    #To give a feature of cut, copy and paste
+    EditMenu.add_command(label = "Cut", command=cut)
+    EditMenu.add_command(label = "Copy", command=copy)
+    EditMenu.add_command(label = "Paste", command=paste)
+
+    MenuBar.add_cascade(label="Edit", menu = EditMenu)
+
+    # Edit Menu Ends
+
+    # Help Menu Starts
+    HelpMenu = Menu(MenuBar, tearoff=0)
+    HelpMenu.add_command(label = "About Notepad", command=about)
+    HelpMenu.add_command(label="Rate Us", command=rate)
+    HelpMenu.add_command(label="Report", command=report)
+    HelpMenu.add_command(label="Guide", command=guide)
+    MenuBar.add_cascade(label="Help", menu=HelpMenu)
+
+    # Help Menu Ends
+
+    root.config(menu=MenuBar)
+
+    #Adding Scrollbar using rules from Tkinter lecture no 22
+    Scroll = Scrollbar(TextArea)
+    Scroll.pack(side=RIGHT,  fill=Y)
+    Scroll.config(command=TextArea.yview)
+    TextArea.config(yscrollcommand=Scroll.set)
+
+    root.mainloop()
+
+
